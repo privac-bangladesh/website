04dd745a13e29fc300dfe25258104a21c8c2652a
Daniel Chung <pikachungg@gmail.com>
2023-06-26 12:50:27 -0400

    yes

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+
+export const Navbar = () => {
+	return (
+		<div className="bg-rose-900 rounded-b-full 2xl:mx-60 xl:mx-36 lg:mx-16 md:mx-10 mx-5">
+			<h1 className="lg:ml-24 md:ml-20 ml-10 py-3">Logo goes here</h1>
+		</div>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,41 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Navbar } from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+			<Navbar/>
+			<div className="
+			2xl:bg-black
+			xl:bg-green-600
+			lg:bg-yellow-400
+			 md:bg-red-500
+			  sm:bg-slate-700 
+				">
+				a
+			</div>
+			{/* Screen one */}
+			<div className="flex">
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+				<div>
+					<h1>Image goes here</h1>
+				</div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+				<div>
+					<h1>Logo goes here</h1>
+					<h1>fights</h1>
+					<h1>cybercrimes</h1>
+				</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+			</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+			{/* <div className="bg-rose-800 h-[1000px]">
+				<div className="bg-neutral-950 h-[500px] rounded-full">
+					<h1>In bagladesh, 10,000,000 Cybercrime cases are reported nation wide.</h1>
+				</div>
+				<div className="bg-rose-800">
+					<h1>But only 19% of victims seek legal help.</h1>
+				</div>
+			</div> */}
     </main>
   )
 }
